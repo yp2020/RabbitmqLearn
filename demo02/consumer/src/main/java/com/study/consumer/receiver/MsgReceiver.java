5baa70ef91290d4445350f1f56bf67d6fabe5481
Wayne <3522373084@qq.com>
2022-02-26 17:25:03 +0800

    work queue 的手动ack 配置

--- demo02/consumer/src/main/java/com/study/consumer/receiver/MsgReceiver.java
+++ demo02/consumer/src/main/java/com/study/consumer/receiver/MsgReceiver.java
@@ -1,8 +1,14 @@
 package com.study.consumer.receiver;
 
+import com.rabbitmq.client.Channel;
 import com.study.consumer.config.RabbitConfig;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * @author yang
@@ -11,16 +17,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class MsgReceiver {
     @RabbitListener(queues = RabbitConfig.QUEUE_NAME)
-    public void handleMsg(String msg){
-        System.out.println("消费者1 : "+msg);
+    public void handleMsg(Message message, Channel channel ) throws IOException {
+        System.out.println("消费者1:" +message.getPayload());
+        //告诉 mq 这条消息已经消费成功了
+        channel.basicAck((Long)message.getHeaders().get(AmqpHeaders.DELIVERY_TAG),false);
+
     }
 
     /**
      * concurrency 为并发数量，也就是开启 10个值线程去消费消息
-     * @param msg
+     * @param 
      */
     @RabbitListener(queues = RabbitConfig.QUEUE_NAME,concurrency = "10")
-    public void handleMsg2(String msg){
-        System.out.println("消费者2:  "+msg);
+    public void handleMsg2(Message message, Channel channel ) throws IOException {
+     //   System.out.println("消费者1:" +message.getPayload());
+        // 拒绝消费消息，true 表示 被拒绝的消息是否要重新回到队列中
+        channel.basicReject((Long)message.getHeaders().get(AmqpHeaders.DELIVERY_TAG),true);
     }
 }
